854f6358f82f492c6bd223025669cb7b298c6c7c
ankitmaurya2233 <102985018+ankitmaurya2233@users.noreply.github.com>
2023-04-14 16:12:52 +0530

    Update html.js

--- project3/html.js
+++ project3/html.js
@@ -1,4 +1,6 @@
-const cont=document.getElementsByClassName("mb-3");
-for (let i = 0; i < cont.length; i++) {
-    cont[i].style.backgroundColor = "red";
-}
+// const cont=document.getElementsByClassName("mb-3");
+// for (let i = 0; i < cont.length; i++) {
+//     cont[i].style.backgroundColor = "red";
+// }
+var l=document.getElementsByTagName('li');
+l[1].textContent ='Ankit';
